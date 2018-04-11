a079d5f5a0dd0997af127aa53fd47d0505a5f353
GC <niushaofeng@mutouw.com>
2018-04-11 14:25:16 +0800

    更新项目列表Cell

--- GangPiaoWang/Module/Project/View/GPWProjectCell.swift
+++ GangPiaoWang/Module/Project/View/GPWProjectCell.swift
@@ -133,7 +133,7 @@ class GPWProjectCell: UITableViewCell {
         }
         staticDateLabel.snp.makeConstraints { (maker) in
             maker.bottom.equalTo(incomeLabel).offset(-3)
-            maker.left.equalTo(contentView.snp.centerX)
+            maker.left.equalTo(contentView.snp.centerX).offset(-20)
         }
         dateLabel.snp.makeConstraints { (maker) in
             maker.centerY.equalTo(staticDateLabel)
